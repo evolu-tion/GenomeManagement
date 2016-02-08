f862aa2c007d0ba75822fda3d6cde80ff76fc603
nattawet <nattawet.sri@mail.kmutt.ac.th>
2016-02-08 19:34:59 +0700

    update

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='GenomeManagement',
+    version='0.1a',
+    author='Nattawet Sriwichai',
+    author_email='nattawet.sri@mail.kmutt.ac.th',
+    packages=['genome_management', 'moongrabber.test'],
+    url='http://pypi.python.org/pypi/xxxxx',
+    license='LICENSE',
+    description='Easy for genome data management based on cassava genome',
+    long_description=open('README.rst').read(),
+    install_requires=[
+
+    ],
+)
