c25a2c49a04133b2772952d68159e6acc90c8f98
Nattawet Sriwichai <nattawet.sri@mail.kmutt.ac.th>
2021-02-12 01:56:14 +0700

    Update seq_manage.py

--- seq_manage.py
+++ seq_manage.py
@@ -68,15 +68,16 @@ class Fasta_manager(object):
 			self.chromosomeSeq[header] = sequence
 			self.chromosomeLength[header] = length
 
+			if show_genome_stat:
+				for i in sequence:
+					i = i.upper()
+					count_bases[i] += 1
+
 		if show_genome_stat:
-      for i in sequence:
-        i = i.upper()
-        count_bases[i] += 1
 			print("Total sequence length:" , "{:0,}".format(sum(count_bases.values())))
 			print("Total ungapped length:" , "{:0,}".format(sum(count_bases.values()) - count_bases['N']))
 			print("Total spanned gaps:", "{:0,}".format(count_bases['N']))
-			print("Number of chromosomes/scaffolds/contigs = ", "{:0,}".format(len(fasta)))
-
+			print("Number of chromosomes/scaffolds/contigs: ", "{:0,}".format(len(fasta)))
 			sumGC = count_bases['G'] + count_bases['C'] + count_bases['S'] + count_bases['Y']/2 + count_bases['K']/2 + count_bases['M']/2 + count_bases['B']*2/3 + count_bases['D']/3 + count_bases['H']/3 + count_bases['V']*2/3 + count_bases['N']/2
 			print("GC content (%):", "{:0,.2f}".format(sumGC * 100 / sum(count_bases.values())))
 			print("N content  (%)", "{:0,.2f}".format(count_bases['N'] * 100 / sum(count_bases.values())))
@@ -89,8 +90,8 @@ class Fasta_manager(object):
 				sum_len += scaffold_len[i]
 				i += 1
 
-			print("N50:", scaffold_len[i-1])
-			print("L50:", i)
+			print("N50:", "{:0,}".format(scaffold_len[i-1]))
+			print("L50:", "{:0,}".format(i))
 
 	def checkChromosome(self, chromosome, start=0, end=1):
 		if(start>end):
